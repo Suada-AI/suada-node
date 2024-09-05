41a017362290ad5d635ee4bc576a09c628528684
Riley Webb <riley.webb392@icloud.com>
2024-09-05 14:54:50 +1000

    Update to v1.0.0-rc12

--- src/models/index.ts
+++
@@ -1,8 +0,0 @@
-/* tslint:disable */
-/* eslint-disable */
-export * from './Completion';
-export * from './CompletionMessagesInner';
-export * from './CompletionResult';
-export * from './CompletionResultChoicesInner';
-export * from './CompletionResultChoicesInnerMessage';
-export * from './CompletionResultUsage';
---
+++ suada-models/index.ts
@@ -0,0 +1,6 @@
+export * from './completion';
+export * from './completion-messages-inner';
+export * from './completion-result';
+export * from './completion-result-choices-inner';
+export * from './completion-result-choices-inner-message';
+export * from './completion-result-usage';
